30b305d3e3679b0138e3e6826ab4b0fa5295cccc
Philip Otieno <iamphilipotieno@gmail.com>
2022-09-28 11:30:57 +0300

    Add schema

--- app/schema.py
+++ app/schema.py
@@ -2,14 +2,14 @@ import graphene
 
 from starwars.schema import Query as snippets_query
 
-# from starwars.schema import Mutation as mutation
+from starwars.schema import Mutation as mutation
 
 
 class Query(snippets_query):
     pass
 
 
-# class Mutation(mutation):
-#     pass
+class Mutation(mutation):
+    pass
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- starwars/schema.py
+++ starwars/schema.py
@@ -2,7 +2,10 @@ import json
 from collections import namedtuple
 
 import requests
-from graphene import Field, List, ObjectType, String
+from graphene import Field, List, ObjectType, String, Mutation
+import graphql_jwt
+import jwt
+from django.contrib.auth import get_user_model
 
 url = "https://swapi.dev/api/people/"
 headers = {}
@@ -18,7 +21,18 @@ def _json_object_hook(d):
 def json2obj(data):
     return json.loads(data, object_hook=_json_object_hook)
 
+"""
+User Type
+"""
+class UserType(ObjectType):
+#   class Meta:
+#     model = get_user_model()
+#     fields = ('id','username', 'password')
+    username = String(required=True)
 
+"""
+People Type
+"""
 class PeopleType(ObjectType):
 
     name = String(description="Persons name")
@@ -32,11 +46,34 @@ class Query(ObjectType):
     people = List(PeopleType)
     search = Field(PeopleType, name=String(required=True))
 
-    def resolve_people(self, info):
+    # Resolve all people
+    def resolve_people(self, info, search=None, **kwargs):
+        # user = info.context.user
+        # if user.is_anonymous:
+        #     raise Exception("Login is required")
         return resp["results"]
-
+        
+    # Resolve person
     def resolve_search(self, info, **args):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Login is required")
         name = args.get("name")
         my_item = next((item for item in resp["results"] if item["name"] == name), None)
 
         return my_item
+
+class AuthMutation(Mutation):
+    token = String()
+    class Arguments:
+        username = String()
+
+    def mutate(self, info , username):
+        user = {"username" : username}
+
+        jwt_token = jwt.encode(user, "secret", algorithm="HS256")
+        res = AuthMutation(token = jwt_token.decode("utf-8"))
+        return res
+
+class Mutation(ObjectType):
+    token_auth = AuthMutation.Field()
